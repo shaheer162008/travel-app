91aec31ae7a8fe330c3c5a84b3d603f03d22f00a
shaheer162008 <144364578+shaheer162008@users.noreply.github.com>
2023-12-19 22:57:58 +0500

    Add files via upload

---
+++ constants/index.ts
@@ -0,0 +1,87 @@
+// NAVIGATION
+export const NAV_LINKS = [
+    { href: '/', key: 'home', label: 'Home' },
+    { href: '/', key: 'how_hilink_work', label: 'How Hilink Work?' },
+    { href: '/', key: 'services', label: 'Services' },
+    { href: '/', key: 'pricing ', label: 'Pricing ' },
+    { href: '/', key: 'contact_us', label: 'Contact Us' },
+  ];
+  
+  // CAMP SECTION
+  export const PEOPLE_URL = [
+    '/person-1.png',
+    '/person-2.png',
+    '/person-3.png',
+    '/person-4.png',
+  ];
+  
+  // FEATURES SECTION
+  export const FEATURES = [
+    {
+      title: 'Dynamic and Fast Loading',
+      icon: '/map.svg',
+      variant: 'green',
+      description:
+        'Harness the power of Next.js to create dynamic web applications with lightning-fast loading times. Our expertise ensures that your website delivers a seamless user experience, reducing bounce rates and keeping your audience engaged.',
+    },
+    {
+      title: 'SEO-Friendly Architecture',
+      icon: '/calendar.svg',
+      variant: 'green',
+      description:
+        "With Next.js, we implement server-side rendering (SSR) and static site generation (SSG), enhancing your website's search engine visibility. Elevate your online presence by ensuring that your content is not only visually appealing but also easily discoverable, driving organic traffic and maximizing your digital reach.",
+    },
+    {
+      title: 'Scalability and Performance Optimization',
+      icon: '/tech.svg',
+      variant: 'green',
+      description:
+        'Our proficiency in Next.js allows us to build scalable applications that grow with your business. Whether you are launching a startup or expanding an enterprise, our web solutions are designed for optimal performance, ensuring a smooth user experience even as your website traffic increases.',
+    },
+    {
+      title: 'Tailored User Interfaces with React Components',
+      icon: '/location.svg',
+      variant: 'orange',
+      description:
+        'By leveraging React components within Next.js, we craft highly interactive and user-friendly interfaces. Tailor your web application to meet the unique needs of your audience, creating an immersive digital experience that captivates visitors and encourages user engagement. With our expertise, your website becomes a dynamic platform that not only showcases your brand but also establishes a lasting connection with your users.',
+    },
+  ];
+  
+  // FOOTER SECTION
+  export const FOOTER_LINKS = [
+    {
+      title: 'Learn More',
+      links: [
+        'About Hilink',
+        'Press Releases',
+        'Environment',
+        'Jobs',
+        'Privacy Policy',
+        'Contact Us',
+      ],
+    },
+    {
+      title: 'Our Community',
+      links: ['Climbing xixixi', 'Hiking hilink', 'Hilink kinthill'],
+    },
+  ];
+  
+  export const FOOTER_CONTACT_INFO = {
+    title: 'Contact Us',
+    links: [
+      { label: 'Admin Officer', value: '123-456-7890' },
+      { label: 'Email Officer', value: 'hilink@akinthil.com' },
+    ],
+  };
+  
+  export const SOCIALS = {
+    title: 'Social',
+    links: [
+      '/facebook.svg',
+      '/instagram.svg',
+      '/twitter.svg',
+      '/youtube.svg',
+      '/wordpress.svg',
+    ],
+  };
+  
